79c9639b2af6571ae25ebb8b81768ba3141dddf2
Robert <67438520+freshskates@users.noreply.github.com>
2020-08-31 14:24:46 -0700

    Add project files.

---
+++ check/check.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+	int n; 
+	std::cout<<"Enter n: ";std::cin>>n;std::cin.ignore(1000,10);
+	for (int i=0;i<n;i++)
+		printf((i%3==0?(i%5==0?"FizzBuzz\n":"Buzz\n"):(i%5==0?"Fizz\n":"%d\n")),i);
+}
